5f5dec3854e780b7b2efadfc41eea7323fae5510
ciper <anmolkarki101@gmail.com>
2023-10-18 10:14:04 +0545

    wrapped up control structures, and how to use them correctly.

--- Beginner/3.BlocksControlStructures/Notes.go
+++ Beginner/3.BlocksControlStructures/Notes.go
@@ -154,9 +154,11 @@ for i := 1; i <= 100; i++ {
 // Example The for-range loop
 evenVals := []int{2, 4, 6, 8, 10, 12}
 
-for i, v := range evenVals {
+for i, v := range evenVals { // you get two loop variables, position 'i' and value 'v' 
 		fmt.Println(i, v)
 	}
+//**If you don’t need to access the key, use an underscore (_) as
+// the variable’s name. This tells Go to ignore the value.
 
 // Running this code produces the following output:
 0 2
@@ -166,3 +168,148 @@ for i, v := range evenVals {
 4 10
 5 12
 
+
+// Example: only for getting keys from Map
+uniqueNames := map[string]bool{"Fred": true, "Raul": true, "Wilma": true}
+for k := range uniqueNames {
+fmt.Println(k)
+}
+
+// Iterating over maps
+// Example 4-15. Map iteration order varies
+m := map[string]int{
+"a": 1,
+"c": 3,
+"b": 2,
+}
+for i := 0; i < 3; i++ {
+fmt.Println("Loop", i)
+for k, v := range m {
+fmt.Println(k, v)
+}
+}
+
+// When you build and run this program, the output varies. Here is one possibility:
+Loop 0 
+c 3
+b 2
+a 1
+Loop 1
+a 1
+c 3
+b 2
+Loop 2
+b 2
+a 1
+c 3
+/*this order of key-value pairs varying is a security feature. 
+  In earlier Go versions, the iteration order for keys in a map was usu‐
+ally (but not always) the same,this caused two problems:
+ I) People would write code that assumed that the order was fixed, and this would
+break at weird times.
+ II) If maps always hash items to the exact same values, and you know that a server is
+storing some user data in a map, you can actually slow down a server with an
+attack called Hash DoS by sending it specially crafted data where all of the keys
+hash to the same bucket.
+
+To prevent these,Go implemented two changes:
+ I) First, they modified the hash algorithm for maps to include a random
+number that’s generated every time a map variable is created.
+ II) Next, they made the
+order of a for-range iteration over a map vary a bit each time the map is looped over.
+These two changes make it far harder to implement a Hash DoS attack.
+
+//EXCEPTION: To make it easier to debug and
+log maps, the formatting functions (like fmt.Println) always out‐
+put maps with their keys in ascending sorted order.
+*/
+
+//Iterating over strings
+// Example Iterating over strings
+samples := []string{"hello", "apple_π!"}
+for _, sample := range samples {
+for i, r := range sample {
+fmt.Println(i, r, string(r))
+}
+fmt.Println()
+// output
+0 104 h
+1 101 e
+2 108 l
+3 108 l
+4 111 o
+
+//Looking at the result for “apple_π!”
+0 97 a
+1 112 p
+2 112 p
+3 108 l
+4 101 e
+5 95 _
+6 960 π //        the value at position 6 is 960. due to for range loop being iterated over runes not bytes
+//First, notice that the first column skips the number 7.
+8 33 !
+
+/*
+// The for-range value is a copy
+	-->	You should be aware that each time the for-range loop iterates over your compound
+			type, it copies the value from the compound type to the value variable. Modifying the
+			value variable will not modify the value in the compound type.
+*/
+
+//Example : Modifying the value doesn’t modify the source
+
+evenVals := []int{2, 4, 6, 8, 10, 12}
+for _, v := range evenVals { // value of 'evenVals' is copied into _,v
+v *= 2
+}
+fmt.Println(evenVals) //[2 4 6 8 10 12]
+
+// Labeling your for statements
+// Example  Labels
+outer:
+for _, outerVal := range outerValues {
+	for _, innerVal := range outerVal {
+		// process innerVal
+		if invalidSituation(innerVal) {
+			continue outer
+		}
+	}
+// here we have code that runs only when all of the
+// innerVal values were sucessfully processed
+}
+
+
+//Choosing the Right for Statement
+// A for-range loop is the best way to walk through a string, since it properly
+// gives you back runes instead of bytes. Favor a for-range loop when iterating over all the contents of an
+// instance of one of the built-in compound types.
+
+/*
+When should you use the complete for loop? The best place for it is when you aren’t
+iterating from the first element to the last element in a compound type. While you
+could use some combination of if, continue, and break within a for-range loop, a
+standard for loop is a clearer way to indicate the start and end of your iteration.
+*/
+
+//switch
+// Example  The switch statement
+words := []string{"a", "cow", "smile", "gopher", "octopus", "anthropologist"}
+
+for _, word := range words {
+		switch size := len(word); size {
+			case 1, 2, 3, 4:
+				fmt.Println(word, "is a short word!")
+			case 5:
+				wordLen := len(word)
+				fmt.Println(word, "is exactly the right length:", wordLen)
+			case 6, 7, 8, 9: //an empty case means nothing happens. ,so no output for octopus(7) and gopher(6)
+			default:
+				fmt.Println(word, "is a long word!")
+		}
+}
+//output
+a is a short word!
+cow is a short word!
+smile is exactly the right length: 5
+anthropologist is a long word!
